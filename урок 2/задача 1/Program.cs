dfb59f51a5d1b448a167293b8a988b58e0453c18
Kuvardina Irina <shokoira@yandex.ru>
2022-12-28 14:23:19 +0300

    задача 1

---
+++ урок 2/задача 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int FindNumber(int num)
+{
+    return num/100%100;
+}
+Console.WriteLine(FindNumber(a));
